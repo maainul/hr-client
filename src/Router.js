627911e8d9a7ce8cc67cc4a325a0fd162179cfe3
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-06-02 11:35:57 +0600

    submit data

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './pages/layouts/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './pages/auth/Register'
 
 
 function Router() {
@@ -9,7 +10,7 @@ function Router() {
             <Navbar />
             <Routes>
                 <Route path='/' element={<div> Home </div>} />
-                <Route path='/register' element={<div> Register </div>} />
+                <Route path='/register' element={<Register />} />
                 <Route path='/login' element={<div> Login </div>} />
                 <Route path='/department/list' element={<div> Department </div>} />
             </Routes>
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
 
 function Register() {
+
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordVerify, setPasswordVerify] = useState('')
+
+    async function register(e) {
+        e.preventDefault();
+        try {
+
+            const registerData = { name, password, passwordVerify }
+            await axios.post('http://localhost:1337/api/v1/auth/register', registerData)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div>
-            <h1>Register</h1>
+            <h1>Register new User</h1>
+            <form onSubmit={register}>
+                <input
+                    type='text'
+                    placeholder='enter your name'
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                />
+                <input
+                    type='password'
+                    placeholder='enter your password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+                <input
+                    type='password'
+                    placeholder='enter your password'
+                    onChange={(e) => setPasswordVerify(e.target.value)}
+                    value={passwordVerify}
+                />
+                <button type='submit'>Register</button>
+            </form>
         </div>
     )
 }
