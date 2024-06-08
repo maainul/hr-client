1e8aac71ca80cd9bc4b94c14520aeece61c3bee4
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-06-08 12:42:32 +0600

    Permission Group Form Update

--- src/pages/auth/group/GroupForm.js
+++ src/pages/auth/group/GroupForm.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 function GroupForm({ getGroupList }) {
@@ -7,14 +7,42 @@ function GroupForm({ getGroupList }) {
     const [name, setName] = useState()
     const [code, setCode] = useState()
     const [permissions, setPermissions] = useState([])
+    const [availablePermissions, setAvailablePermissions] = useState([])
 
+
+    //Fetch Permissions on Component mount
+    useEffect(() => {
+        async function fetchPermissions() {
+            try {
+                const res = await axios.get('http://localhost:1337/api/v1/auth/permission/list')
+                setAvailablePermissions(res.data.plist)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchPermissions()
+    }, [])
+
+    // handle checkbox change
+    const handlePermissionOnChange = (permissionID) => {
+        setPermissions(prev => {
+            if (prev.includes(permissionID)) {
+                return prev.filter(id => id !== permissionID)
+            } else {
+                return [...prev, permissionID]
+            }
+        })
+    }
 
     async function saveGroup(e) {
         e.preventDefault()
         try {
-            const groupData = { name, code, permissions: ["665eb30d7233a01b6e395e41", "665eb3127233a01b6e395e43", "665eb3157233a01b6e395e45", "665eb31a7233a01b6e395e47", "665eb31f7233a01b6e395e49"] }
+            const groupData = { name, code, permissions }
             await axios.post('http://localhost:1337/api/v1/auth/group/create', groupData)
             getGroupList()
+            setCode('')
+            setName('')
+            setPermissions([])
         } catch (error) {
             console.log(error)
         }
@@ -34,6 +62,22 @@ function GroupForm({ getGroupList }) {
                     onChange={(e) => setCode(e.target.value)}
                     value={code}
                 />
+
+                {/* Permissions Data */}
+                <h2>Select Permissions</h2>
+                {availablePermissions.map(permission => (
+                    <div key={permission._id}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                value={permission._id}
+                                checked={permissions.includes(permission._id)}
+                                onChange={() => handlePermissionOnChange(permission._id)}
+                            />
+                            {permission.resource} - {permission.action}
+                        </label>
+                    </div>
+                ))}
 
                 <button type="submit">Submit</button>
             </form>
